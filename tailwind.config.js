3218006d1c239fc02cb948f7f87bc6b48037051f
yousafsabir <yousafsabir430516@gmail.com>
2022-07-16 10:05:20 +0500

    Added loading animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        load: {
+          "0%, 100%": { transform: "translateX(-50%)" },
+          "50%": { transform: "translateX(350%)" },
+        },
+      },
+      animation: {
+        loading: "load 3.5s infinite ease-in-out",
+      },
+    },
   },
   plugins: [],
 };
